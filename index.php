0c17e6ad7c422279f9122588042e3076d0ab837d
tjuy <hafidzp719@gmail.com>
2018-12-04 21:40:35 +0700

    pertama

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>Tugas PHP</title>
+</head>
+<body>
+<?php
+	echo"Nama&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Muhamad Hafidz Pratama";
+	echo "<br />";
+	echo "NIM&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;55201117043";
+	echo "<br />";
+	echo "Jurusan&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Teknik Informatika";
+	echo "<br />";
+	echo "Universitas&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Universitas Nurtanio Bandung";
+?>
+</body>
+</html>
